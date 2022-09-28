5ec82cf820fb6c759cb1dd2886a2a7be8ead31a3
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2022-09-27 19:34:47 -0500

    Add files via upload

--- chat_room .js
+++ chat_room .js
@@ -1,6 +1,15 @@
 var firebaseConfig = {
-
+      apiKey: "AIzaSyBwmqXUIe_q8lAma5RDZb2qY1muyDTTDyg",
+  authDomain: "dog-chat-2975e.firebaseapp.com",
+  projectId: "dog-chat-2975e",
+  storageBucket: "dog-chat-2975e.appspot.com",
+  messagingSenderId: "162693618744",
+  appId: "1:162693618744:web:21e95937533c75cfa55acd"
 };
+    firebase.initializeApp(firebaseConfig);
+    lat=localStorage.getItem("pet");
+    console.log(lat);
+    document.getElementById("hola").innerHTML="hola "+lat+ "Bienvenido a DOG Chat" ;
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Inicio del código
@@ -8,3 +17,6 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //Final del código
       });});}
 getData();
+function cha(){
+      window.location="index.html";
+  }
